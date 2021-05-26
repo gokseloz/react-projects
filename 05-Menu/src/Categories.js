6ce71c2f2009f08db6f7a4e399f4c1bd2f5c4e80
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-05-26 11:26:09 +0200

    Add active-btn class and style

--- 05-Menu/src/Categories.js
+++ 05-Menu/src/Categories.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Categories = ({ items, filterItems }) => {
   let allCategory = items.map((item) => item.category);
   let categories = ["All", ...new Set(allCategory)];
+
+  const [value, setValue] = useState(0)
 
   return (
     <div className="btn-container">
@@ -14,8 +16,9 @@ const Categories = ({ items, filterItems }) => {
             type="button"
             onClick={() => {
               filterItems(category);
+              setValue(index)
             }}
-            className="filter-btn"
+            className={`filter-btn ${index === value && "active-btn"}`}
           >
             {category}
           </button>
